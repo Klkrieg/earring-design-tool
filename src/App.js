bea511144ffd53fd6878fd1411b27a3264713766
Karson Krieg <klkrieg3@gmail.com>
2020-12-18 21:50:07 -0600

    styled components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import {BeadGrid} from './components/BeadGrid';
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Beaded Necklace Tool</h1>
       </header>
+      <BeadGrid />
     </div>
   );
 }
---
+++ src/components/BeadGrid.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BeadGrid = () => {
+    return(
+        <div>
+            <div className={}></div>
+        </div>
+    )
+}
